8181987010b84a1e4355d575c167a3dd84c33f77
kaiem1 <abdurrahmankaiem03@gmail.com>
2024-12-04 03:15:23 +0600

    add phone price chart

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 import './App.css'
 import LineChart from './components/LineChart/LineChart'
 import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
 import PriceOption from './components/PriceOption/PriceOption'
-// import DaisyNav from './components/DaisyNav/DaisyNav'
 
 function App() {
  
@@ -11,9 +11,10 @@ function App() {
   return (
     <>
       <NavBar></NavBar>
-      {/* <DaisyNav></DaisyNav> */}
+      
       <PriceOption></PriceOption>
       <LineChart></LineChart>
+      <Phones></Phones>
      
     </>
   )
--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -4,23 +4,23 @@ const LineChart = () => {
 
     
     const studentData = [
-        { id: 1, name: "A", mathMarks: 78, physicsMarks: 85, chemistryMarks: 92 },
-        { id: 2, name: "B", mathMarks: 85, physicsMarks: 89, chemistryMarks: 76 },
-        { id: 3, name: "C", mathMarks: 92, physicsMarks: 74, chemistryMarks: 88 },
-        { id: 4, name: "D", mathMarks: 74, physicsMarks: 91, chemistryMarks: 82 },
-        { id: 5, name: "E", mathMarks: 88, physicsMarks: 87, chemistryMarks: 90 },
-        { id: 6, name: "F", mathMarks: 69, physicsMarks: 72, chemistryMarks: 78 },
-        { id: 7, name: "G", mathMarks: 95, physicsMarks: 93, chemistryMarks: 97 },
-        { id: 8, name: "H", mathMarks: 81, physicsMarks: 80, chemistryMarks: 85 },
-        { id: 9, name: "I", mathMarks: 76, physicsMarks: 77, chemistryMarks: 84 },
-        { id: 10, name: "J", mathMarks: 89, physicsMarks: 92, chemistryMarks: 91 },
+        { id: 1, name: "A", mathMarks: 8, physicsMarks: 85, chemistryMarks: 2 },
+        { id: 2, name: "B", mathMarks: 85, physicsMarks: 9, chemistryMarks: 6 },
+        { id: 3, name: "C", mathMarks: 2, physicsMarks: 74, chemistryMarks: 88 },
+        { id: 4, name: "D", mathMarks: 74, physicsMarks: 1, chemistryMarks: 2 },
+        { id: 5, name: "E", mathMarks: 8, physicsMarks: 87, chemistryMarks: 100 },
+        { id: 6, name: "F", mathMarks: 69, physicsMarks: 2, chemistryMarks: 78 },
+        { id: 7, name: "G", mathMarks: 5, physicsMarks: 93, chemistryMarks: 7 },
+        { id: 8, name: "H", mathMarks: 81, physicsMarks: 0, chemistryMarks: 85 },
+        { id: 9, name: "I", mathMarks: 6, physicsMarks: 77, chemistryMarks: 4 },
+        { id: 10, name: "J", mathMarks: 89, physicsMarks: 9, chemistryMarks: 91 },
       ];
       
       
 
     return (
         <div>
-            <LChart width={700} height={400} data={studentData}>
+            <LChart width={500} height={400} data={studentData}>
                 <XAxis dataKey="name"></XAxis>
                 <YAxis></YAxis>
             <Line dataKey="mathMarks" stroke='red'></Line>
